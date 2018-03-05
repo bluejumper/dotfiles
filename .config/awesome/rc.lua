6c645d11aed3072ebd95716f6aeb17d90474f244
bluejumper <kbenayak@gmail.com>
2018-03-05 11:38:47 +0000

    awesome: created and configured my own theme. I was able to modify my wallpaper, woo!

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "blue/theme.lua")
+beautiful.init("~/.config/awesome/themes/" .. "blue/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "termite"
